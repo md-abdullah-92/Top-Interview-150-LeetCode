b792778677dddee2d2be9ad5c73470163ef5197b
Md Abdullah Al Mahadi Apurbo <107024764+md-abdullah-92@users.noreply.github.com>
2024-02-17 23:33:36 +0600

    Update 0028-find-the-index-of-the-first-occurrence-in-a-string.cpp

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    long long strStr(string txt, string pat ) {
-            int x = pat.size();                               
-        for (int i = 0; i < txt.size(); i++) {         
-            if (pat == txt.substr(i, x)) {         
-                return i;                                 
-            }
+    long long strStr(string txt, string pat ) {                               
+         auto it = search(txt.begin(), txt.end(), pat.begin(), pat.end());
+        if (it != txt.end()) {
+            return it - txt.begin();
         }
-        return -1;  
+        return -1; 
     }
 };
